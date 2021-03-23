2a1f2c079c5116494fec41ba76b1721b78bcb952
SAGH <42285767+8sagh8@users.noreply.github.com>
2021-03-23 11:57:13 -0400

    feat: route game page

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get(`/newGame`, (req, res)=>{
 });
 
 // listening route
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>{
     console.log(`Server is running at ${PORT}`);
 })
